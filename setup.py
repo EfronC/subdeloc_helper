f8810e448cc57b9b5a405aee266eac236844b04b
Efrain <efrondx@gmail.com>
2024-09-02 11:41:55 -0600

    Update Action

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ if sys.platform == "win32":
     # Windows
     vcpkg_root = os.getenv('VCPKG_ROOT', 'C:\\vcpkg')  # Set your vcpkg path
     libass_head = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'include')
-    libass_lib = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'lib', 'ass.lib')
-    json_lib = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'lib', 'jsoncpp.lib')
+    libass_lib = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'lib')
+    json_lib = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'lib')
     json_head = os.path.join(vcpkg_root, 'installed', 'x64-windows', 'include')
 else:
     # Linux
