9f3b81fb7a685741646b8526b9afaa666868fcd4
Efrain <efrondx@gmail.com>
2024-10-24 11:47:11 -0600

    Add Mac

--- setup.py
+++ setup.py
@@ -54,5 +54,5 @@ setup(
         'License :: OSI Approved :: MIT License',  # License type
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
